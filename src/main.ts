c994d7fe5b71843bd8ec83f6c6b53a4dbbcad3a8
CJ Conti <cjc2818cjc@gmail.com>
2025-11-23 17:58:52 -0500

    Added more park info

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,50 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 
-async function get_weather_data():Promise<string>
-{
-  const api_data:Response = await fetch('https://api.weather.gov/gridpoints/MLB/29,64/forecast');
-  const api_json_data:any = await api_data.json();
-  return JSON.stringify(api_json_data);
-}
-function setup_table(weather_data:any)
-{
 
+async function get_weather_data()
+{
+  if(fetching_data)
+  {
+    const api_data:Response = await fetch('https://api.weather.gov/gridpoints/MLB/29,64/forecast');
+    const api_json_data:any = await api_data.json();
+    return api_json_data;
+  }
+  else
+  {
+    const api_data:Response = await fetch('/sample_weather.json');
+    const api_json_data:any = await api_data.json();
+    return api_json_data;
+  }
+}
+async function get_park_data(park_id:string,index:number=0)
+{
+  if(fetching_data)
+  {
+    const api_data:Response = await fetch(`https://api.themeparks.wiki/v1/entity/${park_id}/live`);
+    const api_json_data:any = await api_data.json();
+    return api_json_data;
+  }
+  else
+  {
+    const api_data:Response = await fetch('/'+local_park_file_links[index]);
+    const api_json_data:any = await api_data.json();
+    return api_json_data;
+  }
+}
+function setup_weather_table(weather_data:any)
+{
   let weather_table_head=document.getElementById("weather_table_head") as HTMLTableElement;
   weather_table_head.innerHTML="";
   let tr:HTMLTableRowElement=document.createElement("tr");
+
+  let th:HTMLTableCellElement=document.createElement("th");
+  th.scope="col";
+  th.innerHTML="Time";
+  tr.appendChild(th);
   for(let day=0;day<7;day++)
   {
-    let period=weather_data["properties"]["periods"][day*2];
-    let th:HTMLTableCellElement=document.createElement("th");
+    th=document.createElement("th");
     th.innerHTML=`Day ${day+1}`;
     th.scope="col";
     tr.appendChild(th);
@@ -27,15 +55,21 @@ function setup_table(weather_data:any)
   let weather_table_body=document.getElementById("weather_table_body") as HTMLTableElement;
   weather_table_body.innerHTML="";
   const total_periods:number=weather_data["properties"]["periods"].length;
+
+  const time_period_names=["Today","Tonight"];
   for(let time_period=0;time_period<2;time_period++)
   {
     tr=document.createElement("tr");
-    let td:HTMLTableCellElement=document.createElement("td");
+    let th:HTMLTableCellElement=document.createElement("th");
+    th.innerHTML=time_period_names[time_period];
+    th.scope="row";
+    tr.appendChild(th);
+
     for(let day=0;day<7;day++)
     {
       let index=2*day+time_period;
       let period=weather_data["properties"]["periods"][index];
-      td=document.createElement("td");
+      let td:HTMLTableCellElement=document.createElement("td");
       td.classList.add("day_info");
       td.innerHTML=`${period["temperature"]}${period["temperatureUnit"]}<br>${period["shortForecast"]}`;
 
@@ -44,15 +78,174 @@ function setup_table(weather_data:any)
     weather_table_body.appendChild(tr);
   }
 }
+function setup_today_weather(weather_data:any)
+{
+  let weather_today_table_head=document.getElementById("weather_today_table_head") as HTMLTableElement;
+  let weather_today_table_body=document.getElementById("weather_today_table_body") as HTMLTableElement;
+
+  let tr:HTMLTableRowElement=document.createElement("tr");
+  let th:HTMLHeadingElement=document.createElement("th");
+  th.innerHTML="Today";
+  tr.appendChild(th);
+
+  th=document.createElement("th");
+  th.innerHTML="Tonight";
+  tr.appendChild(th);
+  weather_today_table_head.appendChild(tr);
+
+  tr=document.createElement("tr");
+  for(let time_period=0;time_period<2;time_period++)
+  {
+    let td:HTMLTableCellElement=document.createElement("td");
+    let period=weather_data["properties"]["periods"][time_period];
+    td.classList.add("day_info");
+    td.innerHTML=`${period["temperature"]}${period["temperatureUnit"]}<br>${period["shortForecast"]}`;
+    tr.appendChild(td);
+  }
+  weather_today_table_body.appendChild(tr);
+}
+
+function find_park_hours(park_data:any):string[]
+{
+  const estOptions:any = {
+        timeZone: 'America/New_York', // EST/EDT time zone
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true // For AM/PM format
+  };
+
+  let earliest_opening_time=Number.MAX_SAFE_INTEGER;
+  let earliest_opening_time_written="TESTING";
+  let latest_closing_time=Number.MIN_SAFE_INTEGER;
+  let latest_closing_time_written="TESTING";
+
+  for(let item of park_data["liveData"])
+  {
+    if(item["entityType"]=="ATTRACTION")
+    {
+      const operating_hours=item["operatingHours"];
+      if(!operating_hours||operating_hours.length==0)
+      {
+        continue;
+      }
+
+      const opening_time:Date=new Date(operating_hours[operating_hours.length-1]["startTime"]);
+      const opening_time_written=opening_time.toLocaleString('en-US', estOptions);
+      if(opening_time.getSeconds()<earliest_opening_time)
+      {
+        earliest_opening_time=opening_time.getSeconds();
+        earliest_opening_time_written=opening_time_written;
+      }
+
+      const closing_time:Date=new Date(operating_hours[operating_hours.length-1]["endTime"]);
+      const closing_time_written=closing_time.toLocaleString('en-US', estOptions);
+      if(closing_time.getSeconds()>latest_closing_time)
+      {
+        latest_closing_time=closing_time.getSeconds();
+        latest_closing_time_written=closing_time_written;
+      }
+    }
+  }
+
+  return [earliest_opening_time_written,latest_closing_time_written];
+}
+function setup_park_hours(park_data:any,park_index:number)
+{
+  let time_range=find_park_hours(park_data);
+  console.log(time_range);
+  console.log(time_range);
+
+
+  let hours_h2=document.createElement("h2");
+  hours_h2.innerHTML=`${park_names[park_index]} Hours`;
+  document.getElementById("park_hours_today")?.appendChild(hours_h2);
+
+  const opening_time:string=time_range[0];
+  const closing_time:string=time_range[1];
+  let park_hours_h2=document.createElement("h2");
+  park_hours_h2.innerHTML=opening_time+" - "+closing_time;
+  document.getElementById("park_hours_today")?.appendChild(park_hours_h2);
+}
+function setup_park_nighttime(park_data:any,park_index:number)
+{
+  let h2=document.createElement("h2");
+  h2.innerHTML=`${park_names[park_index]} Nighttime Shows`;
+  document.getElementById("park_nighttime_today")?.appendChild(h2);
+
+
+  const estOptions:any = {
+        timeZone: 'America/New_York', // EST/EDT time zone
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true // For AM/PM format
+  };
+
+  for(let item of park_data["liveData"])
+  {
+    if(item["entityType"]=="SHOW")
+    {
+      if(item["showtimes"]&&item["showtimes"].length>0&&item["showtimes"].length<=3)
+      {
+        const first_opening_time:Date=new Date(item["showtimes"][0]["startTime"]);
+        const first_opening_hour=first_opening_time.toLocaleTimeString("en",{timeZone:'America/New_York',hour:'numeric',hour12:false});
+        console.log(first_opening_hour);
+        if(parseInt(first_opening_hour)<19)
+        {
+          continue;
+        }
+
+        let h3=document.createElement("h3");
+        h3.innerHTML=item["name"];
+        document.getElementById("park_nighttime_today")?.appendChild(h3);
+        
+        let ul=document.createElement("ul");
+        document.getElementById("park_nighttime_today")?.appendChild(ul);
+        for(let i=0;i<item["showtimes"].length;i++)
+        {
+          const opening_time:Date=new Date(item["showtimes"][i]["startTime"]);
+          const opening_time_written=opening_time.toLocaleString("en",estOptions);
+          const closing_time:Date=new Date(item["showtimes"][i]["endTime"]);
+          const closing_time_written=closing_time.toLocaleString("en",estOptions);
+          let li=document.createElement("li");
+          li.innerHTML=`${opening_time_written} - ${closing_time_written}`;
+          ul.appendChild(li);
+        }
+      }
+    }
+  }
+}
 async function main()
 {
-  let weather_data=JSON.parse(await get_weather_data());
+  if(!fetching_data)
+  {
+    let alert_h1:HTMLHeadingElement=document.createElement("h1");
+    alert_h1.innerHTML="NOT FETCHING DATA. USING SAMPLE DATA";
+    document.getElementById("welcome_div")?.appendChild(alert_h1);
+  }
+  let weather_data=await get_weather_data();
   console.log(weather_data);
-  setup_table(weather_data);
+  setup_weather_table(weather_data);
+  setup_today_weather(weather_data);
+
+  for(let i=0;i<4;i++)
+  {
+    let park_data=await get_park_data(park_keys[i],i);
+    park_arr.push(park_data);
+  }
+  console.log(park_arr[0]);
+  setup_park_hours(park_arr[0],0);
+  setup_park_nighttime(park_arr[0],0);
 
   let welcome_heading=document.getElementById("welcome_heading") as HTMLHeadingElement;
   welcome_heading.innerHTML=`Welcome ${family_name} Family`;
 }
 
+
+const park_names=["Magic Kingdom","Epcot","Disney Hollywood Studios","Animal Kingdom"];
+const park_keys=["75ea578a-adc8-4116-a54d-dccb60765ef9","47f90d2c-e191-4239-a466-5892ef59a88b","288747d1-8b4f-4a64-867e-ea7c9b27bad8","1c84a229-8862-4648-9c71-378ddd2c7693"];
+const local_park_file_links=["magic_kingdom_sample.json","epcot_sample.json","hollywood_studios_sample.json","animal_kingdom_sample.json"];
+
+const fetching_data=false;
+let park_arr:any=[];
 const family_name="Disney";
 main();
